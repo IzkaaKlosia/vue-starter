51bd9d57129651839fa8e0a0f5e65c12e91868f1
Iza Klosinska <klosinskaiza@gmail.com>
2023-04-16 11:18:05 +0200

    Logowanie i wylogowanie

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,44 @@
 <template>
   <div class="my-app">
-    <h1>Hello MWO!</h1>
+    <div v-if="loggedIn">
+      Jesteś zalogowany
+      <br>
+      <button @click="logOut()">Wyloguj</button>
+    </div>
+    <div v-else>
+    <h1>Witaj w systemie do zapisów na zajęcia </h1>
+    Zaloguj się emailem:
+    <input type="email" v-model="email">
+    <button @click="logMeIn()">Wchodzę</button>
+    </div>
   </div>
+
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      email: '',
+      loggedIn: false,
+      password: '',
+      loggedOut: true
+    };
+  },
+  methods: {
+    alertMyEmail() {
+      alert(this.email);
+    },
+    logMeIn(){
+      this.loggedIn=true;
+    },
+    logOut(){
+      this.loggedIn=false;
+      this.email='';
+    }
+  }
+
+
 
 }
 </script>
